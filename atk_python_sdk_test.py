146817a50768856315c0627d0186b787fc8078d9
Ouyang-Jiahong <ouyangjiahong22@outlook.com>
2025-08-20 12:06:50 +0800

    跑通了atk_python_sdk的测试用例，能够获取tle声明的卫星的可见性报告

--- atk_python_sdk_test.py
+++ atk_python_sdk_test.py
@@ -2,41 +2,52 @@ from atk_python_sdk import atkOpen, atkConnect, atkClose
 
 if __name__ == "__main__":
     base = "http://localhost:8080"
-    atkClose(base)
+    # 连接 ATK
+    color_num = 1
     atkOpen(base, "127.0.0.1", 6655)
 
-    # atkConnect(base, "Access", "*/Satellite/Sat2 */Satellite/Sat1 TimePeriod \"1 Jan 2025 00:00:00.00\" \"5 Jan 2025 00:00:00.00\"")
-    # atkConnect(base, "Report_RM", "*/Satellite/Sat2 Style \"Access\" Type Display AccessObject */Satellite/Sat1 TimePeriod \"1 Jan 2025 00:00:00.00\" \"5 Jan 2025 00:00:00.00\" TimeStep 60")
+    # 创建场景、设置场景时间
+    atkConnect(base, "New", "/ Scenario SimpleScenario")
+    atkConnect(base, "SetAnalysisTimePeriod", '* "1 Jan 2025 00:00:00.000" "5 Jan 2025 00:00:00.000"')
+    atkConnect(base, "Animate", "* Reset")
 
-    # atkConnect(base, "AER", "*/Satellite/Sat2 */Satellite/Sat1 TimePeriod \"1 Jan 2025 00:00:00.00\" \"5 Jan 2025 00:00:00.00\"")
-    # atkConnect(base, "Report_RM", "*/Satellite/Sat2 Style \"AER\" Type Display AccessObject */Satellite/Sat1 TimePeriod \"1 Jan 2025 00:00:00.00\" \"5 Jan 2025 00:00:00.00\" TimeStep 60")
+    # 使用 TLE 创建卫星
+    tle1 = "1 99001U 25001A   25197.00000000  .00001200  00000-0  90000-4 0  9997"
+    tle2 = "2 99001  97.4000  45.0000 0009000  90.0000 270.0000 13.20000000  1000"
+    atkConnect(base, "New", "/ Satellite sat_tle")
+    atkConnect(base, "SetState", f'*/Satellite/sat_tle TLE "{tle1}" "{tle2}"')
+    atkConnect(base, "Graphics", f'*/Satellite/sat_tle SetColor {color_num}')
+    color_num = color_num + 1
+    atkConnect(base, "Animate", "* Reset")
 
-    # （1）场景与时间
-    print(atkConnect(base, "New", "/ Scenario SimpleScenario"))
-    atkConnect(base, "SetAnalysisTimePeriod", "* \"1 Jan 2025 00:00:00.00\" \"5 Jan 2025 00:00:00.00\"");
-    atkConnect(base, "Animate", "* Reset");
 
-    # （2）地面站
-    atkConnect(base, "New", "/ Facility GroundStation1");
-    atkConnect(base, "SetPosition", "*/Facility/GroundStation1 Geodetic 39.9 116.4 50.0");
+    # 使用 轨道六根数 创建卫星
+    atkConnect(base, "New", "/ Satellite sat_para")
+    atkConnect(base, "SetState", '*/Satellite/sat_para Classical TwoBody "1 Jan 2025 00:00:00.000" "5 Jan 2025 00:00:00.000" 60 J2000 "1 Jan 2025 00:00:00.000" 6678137 0 28.5 0 0 0')
+    atkConnect(base, "Graphics", f'*/Satellite/sat_para SetColor {color_num}')
+    color_num = color_num + 1
+    atkConnect(base, "Animate", "* Reset")
 
-    # （3）卫星
-    atkConnect(base, "New", "/ Satellite Sat1");
-    atkConnect(base, "SetState", "*/Satellite/Sat1 Classical TwoBody NoProp 60.0 J2000 \"2025-01-01 00:00:00.000\" 6878136.999999994412065 0.0 44.997423212349375 359.818801881724369 0 359.80316033301915");
+    # 创建地面站
+    atkConnect(base, "New", "/ Facility GroundStation1")
+    atkConnect(base, "SetPosition", "*/Facility/GroundStation1 Geodetic 39.9 116.4 50.0")
+    atkConnect(base, "Graphics", f'*/Facility/GroundStation1 SetColor {color_num}')
+    color_num = color_num + 1
+    atkConnect(base, "SetConstraint", '*/Facility/GroundStation1 AzimuthAngle Min -180.0 Max 180.0 ExcludeIntervals')
+    atkConnect(base, "SetConstraint", '*/Facility/GroundStation1 ElevationAngle Min -90.0 Max 90.0 ExcludeIntervals')
+    atkConnect(base, "Animate", "* Reset")
 
-    atkConnect(base, "New", "/ Satellite Sat2");
-    atkConnect(base, "SetState", "*/Satellite/Sat1 Classical TwoBody NoProp 60.0 J2000 \"2025-01-01 00:00:00.000\" 9878136.999999994412065 0.0 44.997423212349375 359.818801881724369 0 359.80316033301915");
+    # 创建地面站传感器
+    atkConnect(base, "New", "/ Facility/GroundStation1/Sensor Sensor1")
+    atkConnect(base, "Define", "*/Facility/GroundStation1/Sensor/Sensor1 Rectangular 25.1 36.8")
+    atkConnect(base, "Point", '*/Facility/GroundStation1/Sensor/Sensor1 Fixed Euler 121 180.0 0.0 0.0')
+    atkConnect(base, "Graphics", f'*/Facility/GroundStation1/Sensor/Sensor1 SetColor {color_num}')
+    color_num = color_num + 1
+    atkConnect(base, "Animate", "* Reset")
 
-    # （4）传感器
-    atkConnect(base, "New", "/ Facility/GroundStation1/Sensor GroundSensor1");
-    atkConnect(base, "Define", "*/Facility/GroundStation1/Sensor/GroundSensor1 SimpleCone 30");
-    atkConnect(base, "Point", "*/Facility/GroundStation1/Sensor/GroundSensor1 Fixed Quaternion 0 0 0 -1");
+    # 获取可见性报告
+    atkConnect(base, "Report_RM", '*/Facility/GroundStation1/Sensor/Sensor1 Style "Access" Type Display AccessObject */Satellite/sat_para TimePeriod "1 Jan 2025 00:00:00.000" "5 Jan 2025 00:00:00.000"')
+    atkConnect(base, "Report_RM", '*/Facility/GroundStation1/Sensor/Sensor1 Style "Access" Type Display AccessObject */Satellite/sat_tle TimePeriod "1 Jan 2025 00:00:00.000" "5 Jan 2025 00:00:00.000"')
 
-    # （5）可见性 + 报表（示例）
-    atkConnect(base, "Animate", "* Reset");
-    atkConnect(base, "Access", "*/Satellite/Sat2 */Satellite/Sat1 TimePeriod \"1 Jan 2025 00:00:00.00\" \"5 Jan 2025 00:00:00.00\"")
-    atkConnect(base, "Report_RM", "*/Satellite/Sat2 Style \"Access\" Type Display AccessObject */Satellite/Sat1 TimePeriod \"1 Jan 2025 00:00:00.00\" \"5 Jan 2025 00:00:00.00\" TimeStep 60")
-
-    # 可选：保存场景
-    atkConnect(base, "Save", "/ *");
+    # 关闭 ATK
     atkClose(base)
